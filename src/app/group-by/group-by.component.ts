0771a6af1117ec92a02906544a6b92e109bae886
Shiva Krishna D <shiva@xcontrol.de>
2024-02-23 13:04:27 +0100

    card styling

--- src/app/group-by/group-by.component.ts
+++ src/app/group-by/group-by.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-group-by',
   standalone: true,
-  imports: [MatGridListModule, MatListModule],
+  imports: [MatGridListModule, MatListModule, MatCardModule],
   templateUrl: './group-by.component.html',
   styleUrl: './group-by.component.scss',
 })
